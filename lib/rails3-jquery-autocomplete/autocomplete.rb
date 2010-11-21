3292d9852b5f5f574f20fe4ff9bfc204dde76555
Benjamin Müller <elchbenny@googlemail.com>
2010-11-21 14:09:48 +0100

    add feature to search on multiple models and fields

--- lib/rails3-jquery-autocomplete/autocomplete.rb
+++ lib/rails3-jquery-autocomplete/autocomplete.rb
@@ -5,36 +5,40 @@ module Rails3JQueryAutocomplete
   # Usage:
   # 
   # class ProductsController < Admin::BaseController
-  #   autocomplete :brand, :name
+  #   autocomplete :brand_and_products_search, [:brand, :product], [:name, :description]
   # end
   #
-  # This will magically generate an action autocomplete_brand_name, so, 
+  # This will magically generate an action autocomplete_brand_and_products_search, so, 
   # don't forget to add it on your routes file
   # 
   #   resources :products do
-  #      get :autocomplete_brand_name, :on => :collection
+  #      get :autocomplete_brand_and_products_search, :on => :collection
   #   end
   #
   # Now, on your view, all you have to do is have a text field like:
   # 
-  #   f.text_field :brand_name, :autocomplete => autocomplete_brand_name_products_path
+  #   f.text_field :brand_or_products_name, :autocomplete => autocomplete_brand_and_products_search_products_path
   #
   #
   module ClassMethods
-    def autocomplete(object, method, options = {})
+    def autocomplete(name, objects, methods, options = {})
 
-      define_method("autocomplete_#{object}_#{method}") do
-
+      define_method("autocomplete_#{name}") do
+        
         term = params[:term]
-
+          
+        items = {}
+          
         if term && !term.empty?
-          items = get_items(:model => get_object(object), \
-            :options => options, :term => term, :method => method) 
+          objects.each do |obj|
+            methods.each do |method|
+              items << get_items(:model => get_object(obj), \
+              :options => options, :term => term, :method => method) 
         else
           items = {}
         end
 
-        render :json => json_for_autocomplete(items, options[:display_value] ||= method)
+        render :json => json_for_autocomplete(items, options[:display_value] ||= methods * "-" )
       end
     end
   end
